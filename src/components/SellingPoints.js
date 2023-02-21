3dd3520b7609ea7408982c83fa63a5ca4408a6fa
Alice Moretti <ali.moret88@gmail.com>
2023-02-21 17:14:48 +0200

    Added animations with intersection observer

--- src/components/SellingPoints.js
+++ src/components/SellingPoints.js
@@ -1,7 +1,8 @@
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { useState } from "react"
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 const SellingPoints = () => {
   const data = useStaticQuery(graphql`
@@ -22,24 +23,38 @@ const SellingPoints = () => {
         }
       }
     }
-  `)
-  const sellingPoints = data.allPrismicSellingpoint.nodes
+  `);
+  const sellingPoints = data.allPrismicSellingpoint.nodes;
 
-  const [style, setStyle] = useState(null)
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: "-100px 0px",
+  });
+  /*
+  const [style, setStyle] = useState(null);
   const applyStyle = () => {
     setStyle({
       transition: "all .5s ease-out",
       transform: "rotateY(360deg)",
-    })
-  }
+    });
+  };
+  */
 
   return (
-    <section className="lg:flex text-center mx-auto max-w-screen-xl py-16">
-      {sellingPoints.map(sellingPoint => {
-        const pathToImage = getImage(sellingPoint.data.icon)
+    <section
+      className="lg:flex text-center mx-auto max-w-screen-xl py-16"
+      ref={ref}
+    >
+      {sellingPoints.map((sellingPoint) => {
+        const pathToImage = getImage(sellingPoint.data.icon);
         return (
           <div className="mx-10 mb-10 md:w-2/3 md:mx-auto lg:w-1/4">
-            <div style={style} onMouseEnter={applyStyle}>
+            <div
+              className={`opacity-0 -translate-y-full transition-all duration-1000 ease-out ${
+                inView ? "translate-y-0 z-20 opacity-100" : ""
+              } 
+    `}
+            >
               <GatsbyImage image={pathToImage} />
             </div>
             <h2 className="text-blue-dark text-lg mb-3">
@@ -47,10 +62,14 @@ const SellingPoints = () => {
             </h2>
             <p className="text-grey">{sellingPoint.data.description.text}</p>
           </div>
-        )
+        );
       })}
     </section>
-  )
-}
+  );
+};
 
-export default SellingPoints
+export default SellingPoints;
+
+/*<div style={style} onMouseEnter={applyStyle}>
+              <GatsbyImage image={pathToImage} />
+            </div>*/
--- src/components/WorkFields.js
+++ src/components/WorkFields.js
@@ -1,7 +1,8 @@
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import Title from "./Title"
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { useInView } from "react-intersection-observer";
+import Title from "./Title";
 
 const WorkFields = () => {
   const data = useStaticQuery(graphql`
@@ -27,17 +28,29 @@ const WorkFields = () => {
         }
       }
     }
-  `)
-  const workFields = data.allPrismicAreeDiCompetenza.nodes
-  console.log(workFields)
+  `);
+  const workFields = data.allPrismicAreeDiCompetenza.nodes;
+  console.log(workFields);
+
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    rootMargin: "-100px 0px",
+  });
   return (
     <section className="text-center py-16" id="competenze">
       <Title title="Aree di competenza" />
-      <div className="mx-auto md:grid md:grid-cols-2 gap-6 lg:grid-cols-3 md:max-w-lg lg:max-w-4xl py-16">
-        {workFields.map(field => {
-          const pathToImage = getImage(field.data.icona)
+      <div
+        className="mx-auto md:grid md:grid-cols-2 gap-6 lg:grid-cols-3 md:max-w-lg lg:max-w-4xl py-16"
+        ref={ref}
+      >
+        {workFields.map((field) => {
+          const pathToImage = getImage(field.data.icona);
           return (
-            <div className="rounded-md shadow-md w-64 md:w-auto mx-auto mb-8 md:mb-0">
+            <div
+              className={`rounded-md shadow-md w-64 md:w-auto mx-auto mb-8 md:mb-0 opacity-0 -translate-y-1/2 transition-all duration-1000 ease-out ${
+                inView ? "translate-y-0 opacity-100" : ""
+              } `}
+            >
               <div className="bg-blue-light rounded-t-md py-10">
                 <GatsbyImage image={pathToImage} />
               </div>
@@ -48,11 +61,11 @@ const WorkFields = () => {
                 <p className="text-grey">{field.data.descrizione.text}</p>
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default WorkFields
+export default WorkFields;
