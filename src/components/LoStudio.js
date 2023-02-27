ed9a10eb365eacdfbab540c751b33ab43cfee8b7
Alice Moretti <ali.moret88@gmail.com>
2023-02-27 10:40:39 +0200

    Added text to LoStudio

--- src/components/LoStudio.js
+++ src/components/LoStudio.js
@@ -1,18 +1,21 @@
-import * as React from "react"
-import Container from "./UI/Container"
-import Title from "./Title"
+import * as React from "react";
+import Container from "./UI/Container";
+import Title from "./Title";
 const LoStudio = () => {
   return (
     <Container>
       <Title title="Lo studio" />
       <p className="text-grey">
-        Lo studio Cassani & Marchetti, specializzato in diritto civilista, nasce
-        a Milano nel 1986. Negli anni, siamo diventati il punto di riferimento
-        per numerose aziende e privati per la risoluzione di contenziosi
-        nell’ambito del diritto civile.
+        Lo <p className="font-bold inline">Studio Cassani & Marchetti</p> è il
+        risultato dell’evoluzione della pluriennale esperienza dei propri
+        professionisti, sempre al passo con i tempi e con le nuove tecnologie,
+        in grado di adattarsi e progredire in una società sempre più esigente.
+        Lavoriamo con passione e{" "}
+        <p className="font-bold inline">professionalità</p>, capaci di prevenire
+        o risolvere i problemi dei nostri clienti.
       </p>
     </Container>
-  )
-}
+  );
+};
 
-export default LoStudio
+export default LoStudio;
