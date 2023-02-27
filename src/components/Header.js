90c60698a7756ea10cd905dd9f0db822113f7b0c
Alice Moretti <ali.moret88@gmail.com>
2023-02-27 10:02:33 +0200

    Changed header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,12 +18,15 @@ const Header = ({ siteTitle }) => (
     <div className="bg-blue-transparent text-center absolute top-0 right-0 bottom-0 left-0">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 md:w-3/4 lg:w-3/5 max-w-screen-md">
         <p className="text-white text-sm sm:text-base font-extrabold uppercase">
-          Studio legale Cassani e Marchetti
+          Studio legale Cassani & Marchetti
         </p>
-        <span className="block w-20 mx-auto mt-4 mb-5 border-solid border-gold border-b-4"></span>
+        <span className="flex justify-center mt-6 -mb-6 mx-auto text-gold text-5xl">
+          "
+        </span>
         <h1 className="text-white leading-normal md:leading-normal lg:leading-normal text-3xl md:text-4xl lg:text-5xl font-medium">
-          I tuoi esperti in diritto civile a Milano e Paderno Dugnano
+          Non esistono problemi, ci sono soltanto soluzioni
         </h1>
+        <p className="text-xs text-white italic mt-3">- André Gide</p>
       </div>
     </div>
   </header>
