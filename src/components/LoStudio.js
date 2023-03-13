49d082f0ae590ead9f2cb7a9c6163ca283bee3d0
Alice Moretti <ali.moret88@gmail.com>
2023-03-13 14:21:09 +0200

    Text update

--- src/components/LoStudio.js
+++ src/components/LoStudio.js
@@ -8,9 +8,9 @@ const LoStudio = () => {
       <p className="text-grey">
         Lo <p className="font-bold inline">Studio Cassani & Marchetti</p> è il
         risultato dell’evoluzione della pluriennale esperienza dei propri
-        professionisti, sempre al passo con i tempi e con le nuove tecnologie,
-        in grado di adattarsi e progredire in una società sempre più esigente.
-        Lavoriamo con passione e{" "}
+        professionisti, costantemente al passo con i tempi e con le nuove
+        tecnologie, in grado di adattarsi e progredire in una società sempre più
+        esigente. Lavoriamo con passione e{" "}
         <p className="font-bold inline">professionalità</p>, capaci di prevenire
         o risolvere i problemi dei nostri clienti.
       </p>
