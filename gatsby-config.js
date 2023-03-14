8af2b2c82c0113647d3d78e1085180892cc50061
Alice Moretti <ali.moret88@gmail.com>
2023-03-14 14:47:26 +0200

    Added iubenda cookie banner

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,61 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "GA-TRACKING_ID", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-iubenda-cookie-footer",
+      options: {
+        iubendaOptions: {
+          askConsentAtCookiePolicyUpdate: true,
+          countryDetection: true,
+          enableLgpd: true,
+          enableUspr: true,
+          floatingPreferencesButtonDisplay: "bottom-right",
+          lgpdAppliesGlobally: false,
+          perPurposeConsent: true,
+          whitelabel: false,
+          cookiePolicyId: 83445824,
+          siteId: 3034025,
+          lang: "it",
+          banner: {
+            acceptButtonDisplay: true,
+            closeButtonDisplay: false,
+            customizeButtonDisplay: true,
+            explicitWithdrawal: true,
+            listPurposes: true,
+            position: "float-top-center",
+            rejectButtonDisplay: true,
+            showPurposesToggles: true,
+          },
+        },
+      },
+    },
 
     /*to use google fonts
     {
